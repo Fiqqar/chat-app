a15cb602eea9ea442ec4bf6489424d6a27921219
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-10-18 20:29:53 +0700

    18-10-24(read and write data)
    done
    -show data on template element

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -91,48 +91,19 @@
         </div>
         <div class="mesgs">
           <div class="msg_history">
-            <div class="incoming_msg">
-              <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+            <div class="incoming_msg" v-for="item in messages">
+              <div class="incoming_msg_img" vfor> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
               <div class="received_msg">
                 <div class="received_withd_msg">
-                  <p>Test which is a new approach to have all
-                    solutions</p>
-                  <span class="time_date"> 11:01 AM    |    June 9</span></div>
-              </div>
-            </div>
-            <div class="outgoing_msg">
-              <div class="sent_msg">
-                <p>Test which is a new approach to have all solutions</p>
-                <span class="time_date"> 11:01 AM    |    June 9</span> </div>
-            </div>
-            <div class="incoming_msg">
-            <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="received_msg">
-                <div class="received_withd_msg">
-                <p>Test, which is a new approach to have</p>
-                <span class="time_date"> 11:01 AM    |    Yesterday</span></div>
-            </div>
-            </div>
-            <div class="outgoing_msg">
-            <div class="sent_msg">
-                <p>Apollo University, Delhi, India Test</p>
-                <span class="time_date"> 11:01 AM    |    Today</span> </div>
-            </div>
-            <div class="incoming_msg">
-            <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-            <div class="received_msg">
-                <div class="received_withd_msg">
-                <p>We work directly with our designers and suppliers,
-                    and sell direct to you, which means quality, exclusive
-                    products, at a price anyone can afford.</p>
-                  <span class="time_date"> 11:01 AM    |    Today</span></div>
+                  <p>{{ item.message}}</p>
+                  <span class="time_date">{{ item.date }}</span></div>
               </div>
             </div>
           </div>
           <div class="type_msg">
             <div class="input_msg_write">
-              <input type="text" @keyup="send_message" v-model="message" class="write_msg" placeholder="Type a message" />
-              <button class="msg_send_btn" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+              <input type="text" @keyup.enter="send_message" v-model="message" class="write_msg" placeholder="Type a message" />
+              <button class="msg_send_btn" @click="send_message" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
             </div>
           </div>
         </div>
@@ -148,13 +119,15 @@ export default {
     name: 'Home',
     props: {
       message: null ,
-      username: "Fiqar"
+      username: "Fiqar",
+      messages: []
     },
     methods: {
       send_message() {
         // Add a new document in collection "Username"
-        db.collection("Fiqqar").doc("Fiqqar").set({
-          message: this.message
+        db.collection("Fiqqar").add({
+          message: this.message,
+          date: new Date()
         })
         .then(() => {
             console.log("Document successfully written!");
@@ -162,7 +135,22 @@ export default {
         .catch((error) => {
             console.error("Error writing document: ", error);
         });
-              }
+        this.message = null;
+        }, 
+      read_message() {
+        let allMessages = [];
+        db.collection("Fiqqar").orderBy("date").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        allMessages.push(doc.data())
+        });
+        this.messages = allMessages;
+});
+      }
+    },
+    created() {
+      this.read_message()
     }
 }
 </script>
